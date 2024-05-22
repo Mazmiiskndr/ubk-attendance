69beb6d1abc2b706df07b4c887a58dd8b27c69ed
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2024-05-22 20:51:08 +0700

    Initial commit

---
+++ config/variables.php
@@ -0,0 +1,29 @@
+<?php
+// Variables
+return [
+    "creatorName" => "UBK - Attendance",
+    "creatorUrl" => "https://pixinvent.com",
+    "templateName" => "UBK - Attendance",
+    "templateSuffix" => "Bootstrap Admin Template",
+    "templateVersion" => "1.0.0",
+    "templateFree" => false,
+    "templateDescription" => "Enhance your employee management system with AttendanceApp, the ultimate Bootstrap 5 Admin Template designed for efficient attendance tracking. Packed with advanced features and a user-friendly interface, AttendanceApp simplifies your attendance management tasks while providing detailed analytics and reporting. Perfect for businesses of all sizes looking to streamline their workforce management.",
+    "templateKeyword" => "attendance tracking, employee attendance, attendance management, Bootstrap 5 admin template, workforce management, attendance analytics, employee time tracking, business management, HR management system, attendance software",
+    "licenseUrl" => "https://themeforest.net/licenses/standard",
+    "livePreview" => "https://demos.pixinvent.com/vuexy-html-admin-template/landing/",
+    "productPage" => "https://1.envato.market/vuexy_admin",
+    "support" => "https://pixinvent.ticksy.com/",
+    "moreThemes" => "https://1.envato.market/pixinvent_portfolio",
+    "documentation" => "https://demos.pixinvent.com/vuexy-html-admin-template/documentation",
+    "generator" => "",
+    "changelog" => "https://demos.pixinvent.com/vuexy/changelog.html",
+    "repository" => "https://github.com/pixinvent/vuexy-html-admin-template",
+    "gitRepo" => "UBK - Attendance",
+    "gitRepoAccess" => "vuexy-html-admin-template",
+    "githubFreeUrl" => "https://tools.pixinvent.com/github/github-access",
+    "facebookUrl" => "https://www.facebook.com/pixinvents/",
+    "twitterUrl" => "https://twitter.com/pixinvents",
+    "githubUrl" => "https://github.com/pixinvent",
+    "dribbbleUrl" => "https://dribbble.com/pixinvent",
+    "instagramUrl" => "https://www.instagram.com/pixinvents/"
+];
