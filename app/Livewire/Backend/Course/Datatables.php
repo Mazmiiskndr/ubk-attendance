375317488abc1ca6b3cc9038d9bee8c486532773
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2024-07-18 07:58:58 +0700

    feat(Datatables.php): add Livewire attribute #[On] for event handling to improve code readability and maintainability feat(Datatables.php): add deleteCourse and deleteBatchCourses methods for handling course deletion feat(Datatables.php): add refreshDataTable method to refresh the data table after deletion fix(form.blade.php): change button text from "Sign in" to "Login" for clarity and consistency

--- app/Livewire/Backend/Course/Datatables.php
+++ app/Livewire/Backend/Course/Datatables.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Backend\Course;
 
 use App\Services\Course\CourseService;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Datatables extends Component
@@ -15,5 +16,38 @@ class Datatables extends Component
     public function getDataTable(CourseService $courseService)
     {
         return $courseService->getCourseDatatables();
+    }
+
+    #[On('confirmCourse')]
+    public function deleteCourse(CourseService $courseService, $courseId)
+    {
+        try {
+            $courseService->deleteCourses(base64_decode($courseId));
+
+            $this->dispatch('show-toast', ['type' => 'success', 'message' => 'Mata Kuliah berhasil hapus!']);
+
+            $this->refreshDataTable();
+        } catch (\Throwable $th) {
+            $this->dispatch('show-toast', ['type' => 'error', 'message' => 'Error : ' . $th->getMessage()]);
+        }
+    }
+
+    #[On('deleteBatchCourses')]
+    public function deleteBatchCourses(CourseService $courseService, $courseIds)
+    {
+        try {
+            $courseService->deleteCourses($courseIds);
+
+            $this->dispatch('show-toast', ['type' => 'success', 'message' => 'Mata Kuliah berhasil hapus!']);
+
+            $this->refreshDataTable();
+        } catch (\Throwable $th) {
+            $this->dispatch('show-toast', ['type' => 'error', 'message' => 'Error : ' . $th->getMessage()]);
+        }
+    }
+
+    public function refreshDataTable()
+    {
+        $this->dispatch('refreshDatatable');
     }
 }
