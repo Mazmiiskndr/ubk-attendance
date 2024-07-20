60c6588f386bedd2a784f9c92e173998e6750d80
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2024-07-20 16:04:40 +0700

    chore(controllers): remove unused controllers HomePage, MiscError, Page2 chore(config): clean up unused MenuServiceProvider from app service providers chore(views): delete unused blade templates and macros file chore(menu): remove horizontalMenu.json and verticalMenu.json as they are no longer needed

--- config/app.php
+++ config/app.php
@@ -157,18 +157,17 @@ return [
 
   'providers' => ServiceProvider::defaultProviders()->merge([
     /*
-         * Package Service Providers...
-         */
+     * Package Service Providers...
+     */
 
     /*
-         * Application Service Providers...
-         */
+     * Application Service Providers...
+     */
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     // App\Providers\BroadcastServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
-    App\Providers\MenuServiceProvider::class,
   ])->toArray(),
 
   /*
