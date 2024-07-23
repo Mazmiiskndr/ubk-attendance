de95274626e5b12d06c0b05903e00f4d331b80f0
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2024-07-23 13:33:05 +0700

    feat: add Livewire components for date and month datatables in Lecture and Student attendance sections feat: create Livewire components DateDatatables and MonthDatatables for Lecture and Student feat: add blade views for date and month datatables in Lecture and Student attendance

---
+++ app/Livewire/Backend/Attendance/Lecture/MonthDatatables.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\Attendance\Lecture;
+
+use Livewire\Component;
+
+class MonthDatatables extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.attendance.lecture.month-datatables');
+    }
+}
---
+++ app/Livewire/Backend/Attendance/Student/MonthDatatables.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\Attendance\Student;
+
+use Livewire\Component;
+
+class MonthDatatables extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.attendance.student.month-datatables');
+    }
+}
