fb6390f0bafdc7989de026dea387947970e895df
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2024-07-25 08:27:16 +0700

    style(date-datatables.blade.php): improve table header styling for better readability and consistency

--- resources/views/livewire/backend/attendance/student/date-datatables.blade.php
+++ resources/views/livewire/backend/attendance/student/date-datatables.blade.php
@@ -2,13 +2,13 @@
     <table class="table table-hover table-bordered table-responsive display" id="myTable" data-route="{{ route('attendances.students.date.getDatatable') }}" aria-describedby="myStudentByDateTables">
         <thead>
             <tr>
-                <th><b>No</b></th>
-                <th><b>Nama Mahasiswa</b></th>
-                <th><b>Jam Masuk</b></th>
-                <th><b>Jam Keluar</b></th>
-                <th><b>Tanggal</b></th>
-                <th><b>Status</b></th>
-                <th><b>Aksi</b></th>
+                <th class="text-center fw-bold">No</th>
+                <th class="text-center fw-bold">Nama</th>
+                <th class="text-center fw-bold">Jam Masuk</th>
+                <th class="text-center fw-bold">Jam Keluar</th>
+                <th class="text-center fw-bold">Tanggal</th>
+                <th class="text-center fw-bold">Status</th>
+                <th class="text-center fw-bold">Aksi</th>
             </tr>
         </thead>
     </table>
